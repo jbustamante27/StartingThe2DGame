fbfcbb4c3783f892f20f381dd4b8de9892c9d805
jbustamante27 <65562918+jbustamante27@users.noreply.github.com>
2024-04-17 01:07:47 -0400

    StartingThe2DGame

---
+++ Assets/GameManager/GameMaster.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NETWORK_ENGINE;
+using System.Linq;
+using System.ComponentModel;
+using System;
+using System.Numerics;
+
+public class GameMaster : NetworkComponent
+{
+    public bool GameStarted = false;
+    public override void HandleMessage(string flag, string value)
+    {
+        if (IsClient && flag == "GAMESTART")
+        {
+            NPM[] Players = GameObject.FindObjectsOfType<NPM>();
+            for (int i = 0; i < Players.Length; i++)
+            {
+                Players[i].gameObject.transform.GetChild(0).GetComponent<Canvas>().enabled = false;
+            }
+        }
+    }
+
+    public override void NetworkedStart()
+    {
+
+    }
+
+    public override IEnumerator SlowUpdate()
+    {
+        while (IsServer && !GameStarted)
+        {
+            //NPM == NetworkPlayerManager every one for every player is stored here
+            NPM[] Players = GameObject.FindObjectsOfType<NPM>();
+
+            if (Players.Length > 1)
+            {
+                int RdyPlayers = 0;
+                for (int i = 0; i < Players.Length; i++)
+                {
+                    if (Players[i].IsReady)
+                    {
+                        RdyPlayers++;
+                    }
+                }
+
+                if (RdyPlayers == Players.Length)
+                {
+                    GameStarted = true;
+                    SendUpdate("GAMESTART", "");
+
+                    for (int i = 0; i < Players.Length; i++)
+                    {
+
+                        GameObject MePlayer = MyCore.NetCreateObject(Players[i].CharSelected, Players[i].Owner, GameObject.Find("P" + (i + 1).ToString() + "Start").transform.position);
+                        MePlayer.GetComponent<PlayerCharacter>().ColorSelected = Players[i].ColorSelected;
+                        MePlayer.GetComponent<PlayerCharacter>().PName = Players[i].PName;
+
+                    }
+                }
+
+
+            }
+
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/PlayerCharacters/PlayerCharacter.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NETWORK_ENGINE;
+using UnityEngine.UI;
+public class PlayerCharacter : NetworkComponent
+{
+    public Text PlayerName;
+    public Material[] MColor;
+    public int ColorSelected = -1;
+    public string PName = "<Default>";
+    public override void HandleMessage(string flag, string value)
+    {
+        if (flag == "COLOR_CHOICE" && IsClient)
+        {
+            ColorSelected = int.Parse(value);
+            gameObject.GetComponent<SpriteRenderer>().color = MColor[ColorSelected].color;
+        }
+
+        if (flag == "PNAME" && IsClient)
+        {
+            //Parsing is deriving a value from a string
+            PName = value;
+            PlayerName.text = PName;
+        }
+
+    }
+
+    public override void NetworkedStart()
+    {
+
+    }
+
+    public override IEnumerator SlowUpdate()
+    {
+        while (IsConnected)
+        {
+
+            if (IsServer)
+            {
+                if (IsDirty)
+                {
+                    SendUpdate("COLOR_CHOICE", ColorSelected.ToString());
+
+                    SendUpdate("PNAME", PName.ToString());
+
+
+                    IsDirty = false;
+                }
+            }
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
